80a741781a8c1e93fbc732f691993e7536b02508
Antonio José <bdez_1997@live.com>
2022-01-17 13:32:34 +0100

    {wip

--- app/src/main/java/com/dam2/primermenu/InitFragment.java
+++ app/src/main/java/com/dam2/primermenu/InitFragment.java
@@ -7,18 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class InitFragment extends Fragment {
-
+    View view;
+    Button btnAbrir;
+    TextView txtPrueba;
     public InitFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_init, container, false);
+        view = inflater.inflate(R.layout.fragment_init, container, false);
         getActivity().setTitle("Inicio");
+        view.findViewById(R.id.txtprueba);
+        view.findViewById(R.id.btnAbrir).setOnClickListener(e -> {
+            txtPrueba.setText("funciona");
+        });
         return view;
     }
+
+
+
 }
--- app/src/main/java/com/dam2/primermenu/MainActivity.java
+++ app/src/main/java/com/dam2/primermenu/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -20,11 +23,13 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawerLayout;
+    Button pepe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
