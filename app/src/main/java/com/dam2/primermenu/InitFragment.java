b0af6074e8da4b72bc32253ee587d6415640b1f7
Antonio José <bdez_1997@live.com>
2022-01-17 12:47:57 +0100

    PrimerMenu y fragments
    Apuntes para la necesidad en eventos posteriores

---
+++ app/src/main/java/com/dam2/primermenu/InitFragment.java
@@ -0,0 +1,24 @@
+package com.dam2.primermenu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InitFragment extends Fragment {
+
+    public InitFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_init, container, false);
+        getActivity().setTitle("Inicio");
+        return view;
+    }
+}
---
+++ app/src/main/java/com/dam2/primermenu/MainActivity.java
@@ -0,0 +1,71 @@
+package com.dam2.primermenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                0,0);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        openFragment(new InitFragment());
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        switch(item.getItemId()){
+            case R.id.opcion1: openFragment(new Opcion1Fragment()); break;
+            case R.id.opcion2: openFragment(new Opcion2Fragment()); break;
+            case R.id.opcion3: openFragment(new Opcion3Fragment()); break;
+            case R.id.salir: salir(); break;
+        }
+
+        return false;
+    }
+
+    private void salir() {
+        finish();
+    }
+}
